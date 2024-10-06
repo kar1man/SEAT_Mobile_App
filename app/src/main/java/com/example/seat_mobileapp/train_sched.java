599f1e766507a1db277d86dbb9dc3d31fd2c0bee
kar1man <163315562+kar1man@users.noreply.github.com>
2024-10-06 22:53:11 +0800

    station picker is now functional. NEXT: work on southbound or northbound track

--- app/src/main/java/com/example/seat_mobileapp/train_sched.java
+++ app/src/main/java/com/example/seat_mobileapp/train_sched.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -32,12 +34,43 @@ public class train_sched extends BaseActivity {
     }
 
     private void stationPicker() {
+        ImageView stationView = findViewById(R.id.train_deets);
         Spinner stationMenu = findViewById(R.id.station_dropdown);
+
         stationMenu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
                 String station = adapterView.getItemAtPosition(position).toString();
-                Toast.makeText(train_sched.this, "Selected Station: " + station, Toast.LENGTH_LONG).show();
+
+                switch (station) {
+                    case "Select Station":
+                        Toast.makeText(getApplicationContext(),"Please select station you want to check!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case "Abad Santos":
+                        stationView.setImageResource(R.drawable.abad_santos_deets);
+                        break;
+                    case "Blumentritt":
+                        stationView.setImageResource(R.drawable.blumentritt_deets);
+                        break;
+                    case "Tayuman":
+                        stationView.setImageResource(R.drawable.tayuman_deets);
+                        break;
+                    case "Bambang":
+                        stationView.setImageResource(R.drawable.bambang_deets);
+                        break;
+                    case "Doroteo Jose":
+                        stationView.setImageResource(R.drawable.djose_deets );
+                        break;
+                    case "Carriedo":
+                        stationView.setImageResource(R.drawable.carriedo_deets);
+                        break;
+                    case "Central Terminal":
+                        stationView.setImageResource(R.drawable.central_deets );
+                        break;
+                    case "United Nations":
+                        stationView.setImageResource(R.drawable.united_nations_deets );
+                        break;
+                }
             }
 
             @Override
@@ -46,7 +79,7 @@ public class train_sched extends BaseActivity {
             }
         });
 
-        String[] stationList = {"Abad Santos", "Bluementritt", "Tayuman", "Bambang", "Doroteo Jose", "Carriedo", "Central Terminal", "United Nations"};
+        String[] stationList = {"Select Station","Abad Santos", "Blumentritt", "Tayuman", "Bambang", "Doroteo Jose", "Carriedo", "Central Terminal", "United Nations"};
 
         ArrayAdapter<String> stationAdapter = new ArrayAdapter<>(this, R.layout.custom_spinner, stationList);
         stationAdapter.setDropDownViewResource(R.layout.custom_spinner);
