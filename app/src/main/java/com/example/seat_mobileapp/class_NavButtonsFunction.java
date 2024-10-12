58b42eacd92a55b2b4225270cb2101dc58f4045a
kar1man <163315562+kar1man@users.noreply.github.com>
2024-10-12 17:43:01 +0800

    added last 3 pages (support, news, about)

--- app/src/main/java/com/example/seat_mobileapp/class_NavButtonsFunction.java
+++ app/src/main/java/com/example/seat_mobileapp/class_NavButtonsFunction.java
@@ -33,8 +33,8 @@ public class class_NavButtonsFunction extends AppCompatActivity {
         // Nav button's functionalities
         setupButton(loc, LocationTracker.class);
         setupButton(sched, TrainSched.class);
-//        setupButton(announce, announce_page.class); --> No page yet
-        setupButton(topUp, TopUp.class); // --> to be complete
+        setupButton(announce, News.class);
+        setupButton(topUp, TopUp.class);
         setupButton(home, HomeDashboard.class);
 
         profileMenu.setOnClickListener(new View.OnClickListener() {
