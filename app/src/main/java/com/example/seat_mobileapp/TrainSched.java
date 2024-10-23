af244eb751a6f7a6d82137f82f15010673d7fe73
leenaardddd <iilxnard@gmail.com>
2024-10-24 01:27:59 +0800

    train sched: main layout, toggle button for direction, train sched buttons with IDs. black void placeholder ng information display gagawin ni aljun. sarap sarap

--- app/src/main/java/com/example/seat_mobileapp/TrainSched.java
+++ app/src/main/java/com/example/seat_mobileapp/TrainSched.java
@@ -26,60 +26,6 @@ public class TrainSched extends class_NavButtons {
             return insets;
         });
         setUpNavBar(TrainSched.class);
-        stationPicker();
-    }
-
-    private void stationPicker() {
-        ImageView stationView = findViewById(R.id.train_deets);
-        Spinner stationMenu = findViewById(R.id.station_dropdown);
-
-        stationMenu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-                String station = adapterView.getItemAtPosition(position).toString();
-
-                switch (station) {
-                    case "Select Station":
-                        Toast.makeText(getApplicationContext(),"Please select station you want to check!", Toast.LENGTH_SHORT).show();
-                        break;
-                    case "Abad Santos":
-                        stationView.setImageResource(R.drawable.station_abad_santos);
-                        break;
-                    case "Blumentritt":
-                        stationView.setImageResource(R.drawable.station_blumentritt);
-                        break;
-                    case "Tayuman":
-                        stationView.setImageResource(R.drawable.station_tayuman);
-                        break;
-                    case "Bambang":
-                        stationView.setImageResource(R.drawable.station_bambang);
-                        break;
-                    case "Doroteo Jose":
-                        stationView.setImageResource(R.drawable.station_doroteo_jose);
-                        break;
-                    case "Carriedo":
-                        stationView.setImageResource(R.drawable.station_carriedo);
-                        break;
-                    case "Central Terminal":
-                        stationView.setImageResource(R.drawable.station_central_terminal);
-                        break;
-                    case "United Nations":
-                        stationView.setImageResource(R.drawable.station_united_nations);
-                        break;
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-        String[] stationList = {"Select Station","Abad Santos", "Blumentritt", "Tayuman", "Bambang", "Doroteo Jose", "Carriedo", "Central Terminal", "United Nations"};
-
-        ArrayAdapter<String> stationAdapter = new ArrayAdapter<>(this, R.layout.custom_station_dropdown, stationList);
-        stationAdapter.setDropDownViewResource(R.layout.custom_station_dropdown);
-        stationMenu.setAdapter(stationAdapter);
     }
 
 
