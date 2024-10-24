5ff862ee0fb1b15401d36a6cb445d45dff2b986b
leenaardddd <iilxnard@gmail.com>
2024-10-25 02:22:48 +0800

    dashboard banner

--- app/src/main/java/com/example/seat_mobileapp/HomeDashboard.java
+++ app/src/main/java/com/example/seat_mobileapp/HomeDashboard.java
@@ -1,11 +1,6 @@
 package com.example.seat_mobileapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-
 import androidx.activity.EdgeToEdge;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,8 +8,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class HomeDashboard extends class_NavButtons {
 
-    ImageButton topUpBtn;
-    Button loanBtn, transcationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,38 +20,5 @@ public class HomeDashboard extends class_NavButtons {
             return insets;
         });
         setUpNavBar(HomeDashboard.class);
-
-        onButtonListener();
-    }
-
-    private void onButtonListener() {
-
-        topUpBtn = findViewById(R.id.topUpButton);
-        topUpBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent topUpIntent = new Intent(HomeDashboard.this, TopUp.class);
-                startActivity(topUpIntent);
-            }
-        });
-
-        loanBtn = findViewById(R.id.loan);
-        loanBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent loanIntent = new Intent(HomeDashboard.this, Loan.class);
-                startActivity(loanIntent);
-            }
-        });
-
-        transcationBtn = findViewById(R.id.otherTransac);
-        transcationBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent transactionIntent = new Intent(HomeDashboard.this, TransacHistory.class);
-                startActivity(transactionIntent);
-            }
-        });
-
     }
 }
