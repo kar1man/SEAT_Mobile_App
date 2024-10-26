7d333990ee30059773e2ecb235d02db5d07cf38a
kar1man <163315562+kar1man@users.noreply.github.com>
2024-10-26 23:13:47 +0800

    topup page revised

--- app/src/main/java/com/example/seat_mobileapp/TopUpComplete.java
+++ app/src/main/java/com/example/seat_mobileapp/TopUpComplete.java
@@ -3,6 +3,7 @@ package com.example.seat_mobileapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -14,7 +15,7 @@ public class TopUpComplete extends class_NavButtons {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ImageButton dashboardBtn;
+        Button dashboardBtn;
 
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -27,13 +28,10 @@ public class TopUpComplete extends class_NavButtons {
         setUpNavBar(TopUpComplete.class); // for navigation buttons
 
 
-        dashboardBtn = findViewById(R.id.dashboard);
-        dashboardBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent backToDashBoard = new Intent(TopUpComplete.this, HomeDashboard.class);
-                startActivity(backToDashBoard);
-            }
+        dashboardBtn = findViewById(R.id.backHomeButton);
+        dashboardBtn.setOnClickListener(v -> {
+            Intent backToDashBoard = new Intent(TopUpComplete.this, HomeDashboard.class);
+            startActivity(backToDashBoard);
         });
 
     }
